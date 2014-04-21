527d72b721d28966866a837de4f20e2a970c9f01
Sampriti Panda <sampdingo@hotmail.com>
2014-04-21 06:38:12 -0400

    Made things more customizablee

--- chat.rb
+++ chat.rb
@@ -1,38 +1,40 @@
-require 'debugger'
 require 'cinch'
 
 require_relative 'se-chatty'
 
-sec = SEChatty.new 'stackexchange.com', ENV['SE_CHAT_EMAIL'], ENV['SE_CHAT_PASSWORD'], 13843
+class Bridge
+  def self.run se_site_id, chatbot_user_id, irc_server, irc_channel, irc_nick="SEBot"
+    sec = SEChatty.new 'stackexchange.com', ENV['SE_CHAT_EMAIL'], ENV['SE_CHAT_PASSWORD'], se_site_id
 
-bot = Cinch::Bot.new do
-  configure do |c|
-    c.server = "cs1692x.moocforums.org"
-    c.nick = "SEBot"
-    c.channels = ["#CS_CS169.1x"]
-  end
-end
-
-bot.on :message do |m|
-  sec.send_message "#{m.user.nick}: #{m.params[1]}"
-end
-
-bot_thread = Thread.new {
-  bot.start
-}
-sec_thread = Thread.new {
-  sec.get_messages { |event|
-    JSON.parse(event.data).each do |room, data|
-      room_number = room.match(/\d+/)[0]
-      if data['e']
-        data['e'].each do |e|
-          p e
-          next if e['user_id'] == 115411 # my chatbot's id
-          bot.Channel("#CS_CS169.1x").send("#{e["user_name"]}: #{e["content"]}")
-        end
+    bot = Cinch::Bot.new do
+      configure do |c|
+        c.server = irc_server
+        c.nick = irc_nick
+        c.channels = [irc_channel]
       end
     end
-  }
-}
-bot_thread.join
-sec_thread.join
+
+    bot.on :message do |m|
+      sec.send_message "#{m.user.nick}: #{m.params[1]}"
+    end
+
+    bot_thread = Thread.new {
+      bot.start
+    }
+    sec_thread = Thread.new {
+      sec.get_messages { |event|
+        JSON.parse(event.data).each do |room, data|
+          room_number = room.match(/\d+/)[0]
+          if data['e']
+            data['e'].each do |e|
+              next if e['user_id'] == chatbot_user_id # my chatbot's id
+              bot.Channel(irc_channel).send("#{e["user_name"]}: #{e["content"]}")
+            end
+          end
+        end
+      }
+    }
+    bot_thread.join
+    sec_thread.join
+  end
+end
---
+++ edx.rb
@@ -0,0 +1,3 @@
+require_relative "chat"
+
+Bridge.run(13843, 115411, "cs1692x.moocforums.org", "#jb_test")
